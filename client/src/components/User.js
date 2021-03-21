f622244aa41304bd1f8fac072ed8f74608137a60
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-20 22:55:07 -0700

    Add button that shows name of all favorite'd events

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -2,10 +2,32 @@
 
 import React from 'react'
 
-export default function User({ name }) {
+export default function User(props) {
+    const [faves, setFaves] = React.useState();
+    let { uid, name } = props.user;
+
+    const getFaves = async () => {
+        try {
+            const res = await fetch(`http://localhost:5000/users/${uid}/favorites`);
+            const allFaves = await res.json();
+
+
+            setFaves(allFaves.join(', '));
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div>
             { name }
+            <button
+                onClick={getFaves}
+            >
+                Favorites
+            </button>
+            { faves }
         </div>
     )
 }
