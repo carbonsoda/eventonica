f1e77538f902b2648799c3c611d1790cbfc60c86
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-20 22:55:26 -0700

    Add getUsers fetch call

--- client/src/components/UsersView.js
+++ client/src/components/UsersView.js
@@ -3,13 +3,31 @@ import React from 'react';
 import User from './User';
 
 export default function UsersView() {
+    const [users, setUsers] = React.useState([]);
+    const [faveCount, setFaveCount] = React.useState(0);
+
+    const getUsers = async () => {
+        try {
+            const res = await fetch('http://localhost:5000/users');
+            const allUsers = await res.json();
+            console.log(allUsers);
+            setUsers(allUsers);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    React.useEffect(() => {
+        getUsers();
+    }, []);
 
     return (
         <div>
-            <User name='Jade' />
-            <User name='Anna' />
-            <User name='Tanya' />
-            <User name='Bees' />            
+            {
+                users.map((user) => (
+                    <User user={user} />
+                ))
+            }          
         </div>
     )
 }
