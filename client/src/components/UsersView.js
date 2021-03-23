254973ed53e74a1605ee48c8e558503498513538
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-22 18:56:17 -0700

    Apply prettier

--- client/src/components/UsersView.js
+++ client/src/components/UsersView.js
@@ -1,74 +1,57 @@
 // show all users
-import React from 'react';
-import User from './User';
+import React from "react";
+import User from "./User";
 
 export default function UsersView() {
-    const [users, setUsers] = React.useState([]);
-    const [faveCount, setFaveCount] = React.useState(0);
-    const [newUser, setNewUser] = React.useState('');
+  const [users, setUsers] = React.useState([]);
+  const [faveCount, setFaveCount] = React.useState(0);
+  const [newUser, setNewUser] = React.useState("");
 
-    const getUsers = async () => {
-        fetch('http://localhost:5000/users')
-            .then(res => res.json())
-            .then(allUsers => setUsers(allUsers))
-            .catch(e => console.error(e.stack));
-    }
+  const getUsers = async () => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((allUsers) => setUsers(allUsers))
+      .catch((e) => console.error(e.stack));
+  };
 
-    const addUser = async () => {
+  const addUser = async () => {
+    const inputBody = { name: newUser, email: "" };
 
-        const inputBody = {'name': newUser, 'email': ''};
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(inputBody),
+    })
+      .then((res) => res.json())
+      .then(() => getUsers())
+      .catch((e) => console.error(e.stack));
+  };
 
-        fetch('http://localhost:5000/users',
-            {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(inputBody)
-            }
-        )
-            .then(res => res.json())
-            .then(() => getUsers())
-            .catch(e => console.error(e.stack));
-    }
+  // TODO: implement as a button
+  const deleteUser = async (id) => {
+    fetch(`http://localhost:5000/users/${id}`, { method: "DELETE" })
+      .then(() => setUsers(users.filter((user) => user.uid !== id)))
+      .then(() => getUsers())
+      .catch((e) => console.error(e.stack));
+  };
 
-    // TODO: implement as a button
-    const deleteUser = async (id) => {
-        fetch(`http://localhost:5000/users/${id}`,
-            { method: 'DELETE' }
-        )
-            .then(() => setUsers(
-                users.filter(user => user.uid !== id)
-            ))
-            .then(() => getUsers())
-            .catch(e => console.error(e.stack));
-    }
+  React.useEffect(() => {
+    getUsers();
+  }, []);
 
-    React.useEffect(() => {
-        getUsers();
-    }, []);
-
-    return (
-        <>
-            <div>
-                {
-                    users.map((user) => (
-                        <div key={ user.uid }>
-                            <User user={ user } />
-                        </div>
-                    ))
-                }
-            </div>
-            <div>
-                <input
-                    type="text"
-                    onChange={e => setNewUser(e.target.value)}
-                />
-                <button
-                    onClick={addUser}
-                >
-                    Add User
-                </button>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        {users.map((user) => (
+          <div key={user.uid}>
+            <User user={user} />
+          </div>
+        ))}
+      </div>
+      <div>
+        <input type="text" onChange={(e) => setNewUser(e.target.value)} />
+        <button onClick={addUser}>Add User</button>
+      </div>
+    </>
+  );
 }
-
